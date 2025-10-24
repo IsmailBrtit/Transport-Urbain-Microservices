7aff151c6ac0dfde08e7fe6f28e75e065bf5d3bb
Wingui <smailber28@gmail.com>
2025-10-24 12:48:06 +0100

    first

---
+++ abonnements-service/abonnements-service/src/main/java/com/example/abonnements_service/AbonnementsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.abonnements_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbonnementsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbonnementsServiceApplication.class, args);
+	}
+
+}
---
+++ abonnements-service/abonnements-service/src/test/java/com/example/abonnements_service/AbonnementsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abonnements_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbonnementsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
