be8d1f9f8f630185d8109e0bda1acacec8c091a7
Yasser Nadifi <yassernadifi2003@gmail.com>
2025-11-13 11:24:09 +0100

    auth test service works, so does rbac

---
+++ auth-test-service/src/main/java/Transport_Urbain_Microservices/auth_test_service/AuthTestServiceApplication.java
@@ -0,0 +1,13 @@
+package Transport_Urbain_Microservices.auth_test_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthTestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthTestServiceApplication.class, args);
+	}
+
+}
---
+++ auth-test-service/src/test/java/Transport_Urbain_Microservices/auth_test_service/AuthTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Transport_Urbain_Microservices.auth_test_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthTestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
